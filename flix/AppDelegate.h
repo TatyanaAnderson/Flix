773647c536513dd3a01a2a4cff3578a9c51685b1
TatyanaAnderson <42578270+TatyanaAnderson@users.noreply.github.com>
2022-01-31 19:52:36 -0600

    Added Flix project

---
+++ flix/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  flix
+//
+//  Created by Tatyana Anderson on 1/24/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
